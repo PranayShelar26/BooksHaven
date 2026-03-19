2a9f2bf9a8e689dcff9f562b181a8ce4875d7302
Pranay Shelar <shelarpranay2611@gmail.com>
2026-03-19 05:23:01 +0000

    Add babel-plugin and simplify vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,24 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    react(),
     tailwindcss(),
   ],
   build: {
-    outDir: 'dist',  // ← Change from 'build' to 'dist'
+    outDir: 'dist',
     sourcemap: true,
     minify: false,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'js/[name].[hash].js',
-        chunkFileNames: 'js/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]'
-      }
-    }
   },
   server: {
     proxy: {
